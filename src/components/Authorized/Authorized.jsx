64113300cf6abcb9281dfc396fd93ca882985e7b
蒋赢 <jiangying@romens.cn>
2020-10-26 22:25:21 +0800

    角色管理、用户管理接口对接

--- src/components/Authorized/Authorized.jsx
+++ src/components/Authorized/Authorized.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import { Result } from 'antd';
 import check from './CheckPermissions';
 
-const Authorized = ({
-  children,
-  authority,
-  noMatch = (
-    <Result
-      status="403"
-      title="403"
-      subTitle="Sorry, you are not authorized to access this page."
-    />
-  ),
-}) => {
+const noMatchC = (
+  <Result status="403" title="403" subTitle="Sorry, you are not authorized to access this page." />
+);
+const Authorized = ({ children, authority, noMatch = noMatchC }) => {
   const childrenRender = typeof children === 'undefined' ? null : children;
   const dom = check(authority, childrenRender, noMatch);
   return <>{dom}</>;
